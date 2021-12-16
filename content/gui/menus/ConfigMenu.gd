02678ae8df75dbc78b91cc920a5141e5c9a96d9a
CR1337 <captain_1@gmx.de>
2021-12-16 14:47:13 +0100

    angle selection works now

--- content/gui/menus/ConfigMenu.gd
+++ content/gui/menus/ConfigMenu.gd
@@ -2,22 +2,49 @@ extends Node2D
 
 onready var addressLineEdit = $Background/MarginContainer/VBoxContainer/AddressVBoxContainer/AddressLineEdit
 onready var portLineEdit = $Background/MarginContainer/VBoxContainer/PortVBoxContainer/PortLineEdit
-onready var angleOptionButton = $Background/MarginContainer/VBoxContainer/AngleVBoxContainer/AngleOptionButton
 onready var okButton = $Background/MarginContainer/VBoxContainer/ButtonsHBoxContainer/OkButton
+
+onready var CheckBox0 = $Background/MarginContainer/VBoxContainer/AngleVBoxContainer/HBoxContainer/CheckBox0
+onready var CheckBox90 = $Background/MarginContainer/VBoxContainer/AngleVBoxContainer/HBoxContainer/CheckBox90
+onready var CheckBox180 = $Background/MarginContainer/VBoxContainer/AngleVBoxContainer/HBoxContainer/CheckBox180
+onready var CheckBox270 = $Background/MarginContainer/VBoxContainer/AngleVBoxContainer/HBoxContainer/CheckBox270
+
+onready var CheckBoxes = [
+	$Background/MarginContainer/VBoxContainer/AngleVBoxContainer/HBoxContainer/CheckBox0,
+	$Background/MarginContainer/VBoxContainer/AngleVBoxContainer/HBoxContainer/CheckBox90,
+	$Background/MarginContainer/VBoxContainer/AngleVBoxContainer/HBoxContainer/CheckBox180,
+	$Background/MarginContainer/VBoxContainer/AngleVBoxContainer/HBoxContainer/CheckBox270
+]
 
 func _ready():
 	pass
+	
+func _set_selected_angle(angle):
+	var index = angle / 90
+	for checkBox in CheckBoxes:
+		checkBox.pressed = false
+	CheckBoxes[index].pressed = true
+	
+func _get_selected_angle():
+	if CheckBoxes[0].pressed:
+		return 0
+	elif CheckBoxes[1].pressed:
+		return 90
+	elif CheckBoxes[2].pressed:
+		return 180
+	elif CheckBoxes[3].pressed:
+		return 270
 
 func refresh():
 	Config.load_from_file()
 	addressLineEdit.text = Config.serverAddress
 	portLineEdit.text = Config.serverPort
-	angleOptionButton.text = str(Config.imageRotationAngle)
+	_set_selected_angle(Config.imageRotationAngle)
 
 func _on_OkButton_pressed():
 	Config.serverAddress = addressLineEdit.text
 	Config.serverPort = portLineEdit.text
-	Config.imageRotationAngle = int(angleOptionButton.text)
+	Config.imageRotationAngle = _get_selected_angle()
 	Config.store_to_file()
 	ViewportManager.change_to_last()
 	
@@ -47,3 +74,17 @@ func _on_PortLineEdit_text_changed(new_text):
 
 
 
+func _on_CheckBox0_pressed():
+	_set_selected_angle(0)
+
+
+func _on_CheckBox90_pressed():
+	_set_selected_angle(90)
+
+
+func _on_CheckBox180_pressed():
+	_set_selected_angle(180)
+
+
+func _on_CheckBox270_pressed():
+	_set_selected_angle(270)
