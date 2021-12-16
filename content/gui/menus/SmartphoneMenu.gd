4aa97f9e6e7e766b3a450cbadc500224ea2cd99a
CR1337 <captain_1@gmx.de>
2021-12-16 15:31:09 +0100

    designed StartMenu

--- content/gui/menus/SmartphoneMenu.gd
+++ content/gui/menus/SmartphoneMenu.gd
@@ -17,3 +17,7 @@ func _notification(what):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
