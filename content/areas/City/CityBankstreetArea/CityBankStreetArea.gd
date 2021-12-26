c29b8a503355821b04faf561e0c0dd2e3c599dfb
Jessica <jessica.dierking@student.hpi.uni-potsdam.de>
2021-12-26 21:53:30 +0100

    'Finished' City Areas and colored input UI in very ugly grey

--- content/areas/City/CityBankstreetArea/CityBankStreetArea.gd
+++ content/areas/City/CityBankstreetArea/CityBankStreetArea.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-export (NodePath) var leaveBottomAreaPath
-export (Vector2) var leaveBottomPlayerPosition
-
 export (NodePath) var leaveRightAreaPath
 export (Vector2) var leaveRightPlayerPosition
 
@@ -10,12 +7,9 @@ export (NodePath) var leaveTopAreaPath
 export (Vector2) var leaveTopPlayerPosition
 
 func _ready():
-	$LeaveBottomTrigger.targetArea = get_node(leaveBottomAreaPath)
-	$LeaveBottomTrigger.newPlayerPosition = leaveBottomPlayerPosition
-	
+
 	$LeaveRightTrigger.targetArea = get_node(leaveRightAreaPath)
 	$LeaveRightTrigger.newPlayerPosition = leaveRightPlayerPosition
 	
 	$LeaveTopTrigger.targetArea = get_node(leaveTopAreaPath)
 	$LeaveTopTrigger.newPlayerPosition = leaveTopPlayerPosition
-
--- content/areas/City/CityStorestreetArea/CityStoreStreetArea.gd
+++ content/areas/City/CityStorestreetArea/CityStoreStreetArea.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-export (NodePath) var leaveBottomAreaPath
-export (Vector2) var leaveBottomPlayerPosition
-
 export (NodePath) var leaveLeftAreaPath
 export (Vector2) var leaveLeftPlayerPosition
 
@@ -10,8 +7,6 @@ export (NodePath) var leaveTopAreaPath
 export (Vector2) var leaveTopPlayerPosition
 
 func _ready():
-	$LeaveBottomTrigger.targetArea = get_node(leaveBottomAreaPath)
-	$LeaveBottomTrigger.newPlayerPosition = leaveBottomPlayerPosition
 	
 	$LeaveLeftTrigger.targetArea = get_node(leaveLeftAreaPath)
 	$LeaveLeftTrigger.newPlayerPosition = leaveLeftPlayerPosition
