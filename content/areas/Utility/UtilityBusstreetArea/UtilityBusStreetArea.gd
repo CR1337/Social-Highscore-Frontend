a9ba366fd05c5dda810a2724a005f9863c802b17
CR1337 <captain_1@gmx.de>
2022-01-04 16:10:32 +0100

    added hospital

--- content/areas/Utility/UtilityBusstreetArea/UtilityBusStreetArea.gd
+++ content/areas/Utility/UtilityBusstreetArea/UtilityBusStreetArea.gd
@@ -3,11 +3,17 @@ extends Node2D
 export (NodePath) var leaveLeftAreaPath
 export (Vector2) var leaveLeftPlayerPosition
 
+export (NodePath) var leaveHospitalAreaPath
+export (Vector2) var leaveHospitalPlayerPosition
+
 export (NodePath) var BusOverlayPath
 
 func _ready():
 	$LeaveLeftTrigger.targetArea = get_node(leaveLeftAreaPath)
 	$LeaveLeftTrigger.newPlayerPosition = leaveLeftPlayerPosition
+
+	$LeaveHospitalTrigger.targetArea = get_node(leaveHospitalAreaPath)
+	$LeaveHospitalTrigger.newPlayerPosition = leaveHospitalPlayerPosition
 	
 	$BusTrigger.targetOverlay = get_node(BusOverlayPath)
 	
