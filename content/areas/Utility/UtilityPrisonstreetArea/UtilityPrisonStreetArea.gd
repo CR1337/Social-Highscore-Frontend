6b43f4f2eefc6492e48a7735d05d0e55f7f53e9d
CR1337 <captain_1@gmx.de>
2022-01-04 14:52:53 +0100

    added prison

--- content/areas/Utility/UtilityPrisonstreetArea/UtilityPrisonStreetArea.gd
+++ content/areas/Utility/UtilityPrisonstreetArea/UtilityPrisonStreetArea.gd
@@ -3,6 +3,12 @@ extends Node2D
 export (NodePath) var leaveRightAreaPath
 export (Vector2) var leaveRightPlayerPosition
 
+export (NodePath) var leavePrisonAreaPath
+export (Vector2) var leavePrisonPlayerPosition
+
 func _ready():
 	$LeaveRightTrigger.targetArea = get_node(leaveRightAreaPath)
 	$LeaveRightTrigger.newPlayerPosition = leaveRightPlayerPosition
+
+	$LeavePrisonTrigger.targetArea = get_node(leavePrisonAreaPath)
+	$LeavePrisonTrigger.newPlayerPosition = leavePrisonPlayerPosition
