7654deb7508beef66e38842a3096a03da4822745
CR1337 <captain_1@gmx.de>
2022-01-04 11:22:05 +0100

    added pharmacy

--- content/areas/Living/LivingPharmacystreetArea/LivingPharmacystreetArea.gd
+++ content/areas/Living/LivingPharmacystreetArea/LivingPharmacystreetArea.gd
@@ -3,10 +3,15 @@ extends Node2D
 export (NodePath) var leaveRightAreaPath
 export (Vector2) var leaveRightPlayerPosition
 
+export (NodePath) var leavePharmacyAreaPath
+export (Vector2) var leavePharmacyPlayerPosition
 
 var active = false
 
 func _ready():
 	$LeaveRightTrigger.targetArea = get_node(leaveRightAreaPath)
 	$LeaveRightTrigger.newPlayerPosition = leaveRightPlayerPosition
+
+	$LeavePharmacyTrigger.targetArea = get_node(leavePharmacyAreaPath)
+	$LeavePharmacyTrigger.newPlayerPosition = leavePharmacyPlayerPosition
 
