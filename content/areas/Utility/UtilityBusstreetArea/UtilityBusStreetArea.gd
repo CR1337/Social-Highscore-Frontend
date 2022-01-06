80aa26e58ce38f8f981dfbe6aca854425095d65b
CR1337 <captain_1@gmx.de>
2022-01-06 20:10:00 +0100

    added mall

--- content/areas/Utility/UtilityBusstreetArea/UtilityBusStreetArea.gd
+++ content/areas/Utility/UtilityBusstreetArea/UtilityBusStreetArea.gd
@@ -6,6 +6,9 @@ export (Vector2) var leaveLeftPlayerPosition
 export (NodePath) var leaveHospitalAreaPath
 export (Vector2) var leaveHospitalPlayerPosition
 
+export (NodePath) var leaveMallAreaPath
+export (Vector2) var leaveMallPlayerPosition
+
 export (NodePath) var BusOverlayPath
 
 func _ready():
@@ -14,6 +17,9 @@ func _ready():
 
 	$LeaveHospitalTrigger.targetArea = get_node(leaveHospitalAreaPath)
 	$LeaveHospitalTrigger.newPlayerPosition = leaveHospitalPlayerPosition
+
+	$LeaveMallTrigger.targetArea = get_node(leaveMallAreaPath)
+	$LeaveMallTrigger.newPlayerPosition = leaveMallPlayerPosition
 	
 	$BusTrigger.targetOverlay = get_node(BusOverlayPath)
 	
