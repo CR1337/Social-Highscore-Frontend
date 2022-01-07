86f851960310af256ccd5eb68d3d27e658b2767f
CR1337 <captain_1@gmx.de>
2022-01-07 11:36:40 +0100

    added office

--- content/areas/City/CityBankstreetArea/CityBankStreetArea.gd
+++ content/areas/City/CityBankstreetArea/CityBankStreetArea.gd
@@ -6,6 +6,12 @@ export (Vector2) var leaveRightPlayerPosition
 export (NodePath) var leaveTopAreaPath
 export (Vector2) var leaveTopPlayerPosition
 
+export (NodePath) var leaveOfficeAreaPath
+export (Vector2) var leaveOfficePlayerPosition
+
+export (NodePath) var leaveBankAreaPath
+export (Vector2) var leaveBankPlayerPosition
+
 func _ready():
 
 	$LeaveRightTrigger.targetArea = get_node(leaveRightAreaPath)
@@ -13,6 +19,12 @@ func _ready():
 	
 	$LeaveTopTrigger.targetArea = get_node(leaveTopAreaPath)
 	$LeaveTopTrigger.newPlayerPosition = leaveTopPlayerPosition
+	
+	$LeaveOfficeTrigger.targetArea = get_node(leaveOfficeAreaPath)
+	$LeaveOfficeTrigger.newPlayerPosition = leaveOfficePlayerPosition
+	
+	$LeaveBankTrigger.targetArea = get_node(leaveBankAreaPath)
+	$LeaveBankTrigger.newPlayerPosition = leaveBankPlayerPosition
 
 	cars = {
 		29: $car_29,
