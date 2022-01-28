bbc4d8aa84733f9240a9afc75c18e68f1400058a
CR1337 <captain_1@gmx.de>
2022-01-28 15:53:04 +0100

    NewsController can now handle multiple running reactions, news with a currently running reaction are not deleted

--- content/singletons/NewsController.gd
+++ content/singletons/NewsController.gd
@@ -4,9 +4,8 @@ extends Node
 signal sig_publish_news(title, text, preferred_emotion, lifetime)
 
 var news = []
-var _image_processor_job_id = -1
-var _news_index_reacting_on = -1
 const _max_n_news = 3
+var _ongoing_reactions = {}
 
 func persistent_state():
 	return {
@@ -31,8 +30,8 @@ func publish_news(title, text, preferred_emotion, lifetime):
 	
 func react_on(news_index, want_to_react):
 	if want_to_react:
-		_image_processor_job_id = ImageProcessor.analyze()
-		_news_index_reacting_on = news_index
+		var job_id = ImageProcessor.analyze()
+		_ongoing_reactions[job_id] = news_index
 	else:
 		var news_reacting_on = news[news_index]
 		news_reacting_on['reacted_on'] = true
@@ -41,13 +40,13 @@ func react_on(news_index, want_to_react):
 		)
 
 func _on_image_processing_done(parsed_response, job_id, image, raw_image, b64_image):
-	if job_id == _image_processor_job_id:
-		_image_processor_job_id = -1
-		_handle_reaction(b64_image, parsed_response['dominant_emotion'])
+	if _ongoing_reactions.has(job_id):
+		_handle_reaction(b64_image, parsed_response['dominant_emotion'], _ongoing_reactions[job_id])
+		_ongoing_reactions.erase(job_id)
 		
-func _handle_reaction(b64_image, emotion):
+func _handle_reaction(b64_image, emotion, news_index_reacting_on):
 	var delta_score;
-	var news_reacting_on = news[_news_index_reacting_on]
+	var news_reacting_on = news[news_index_reacting_on]
 	news_reacting_on['reacted_on'] = true
 	if emotion != news_reacting_on['preferred_emotion']:
 		delta_score = -30
@@ -57,12 +56,13 @@ func _handle_reaction(b64_image, emotion):
 		delta_score, news_reacting_on['title'], b64_image,
 		emotion, news_reacting_on['preferred_emotion']
 	)
-	_news_index_reacting_on = -1;
 	
-func _oldest_news():
+func _oldest_news(skip_running_reactions):
 	var n = news[0]
 	for i in len(news) - 1:
 		if news[i + 1]['age'] > n['age']:
+			if skip_running_reactions and (i + 1) in _ongoing_reactions.values():
+				continue
 			n = news[i + 1]
 	return n
 	
@@ -75,7 +75,7 @@ func _erase_news(news_to_erase):
 
 func _on_publish_news(title, text, preferred_emotion, lifetime):
 	while len(news) >= _max_n_news:
-		_erase_news(_oldest_news())
+		_erase_news(_oldest_news(true))
 	news.append({
 		'title': title,
 		'text': text,
